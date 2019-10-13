1d34eb4119fba02698d6867b951d73d55fe1d386
Santiago Salazar <s.salazara@uniandes.edu.co>
2019-10-13 01:05:17 -0500

    Más cambios para que se visualice mejor. Toca revisar ajustes para móviles y demás y ponerlo todo con Router y Links de React. Toca mirar más el contraste del verde y si se pone más oscuro.

--- imports/ui/Home/Home.js
+++ imports/ui/Home/Home.js
@@ -11,17 +11,17 @@ export default class Home extends Component {
         return (
             <div className="container host d-flex align-items-center">
                 <div className="row align-items-center justify-content-center">
-                    <div className="col-12 col-md-6 text-left">
+                    <div className="col-12 col-md-6 text-left my-5 my-md-0">
                         <h2>¡Calcula tu huella y compite con tus amigos!</h2>
-                        <p className="py-4 text-muted text-left">
+                        <p className="py-4 text-muted">
                             La mejor forma de saber cuál es tu huella de carbono de lo que usas a diario.
                         </p>
                         <div className="d-flex justify-content-end">
-                            <button className="but" onClick={this.start}>Inicia Ahora</button>
+                            <button className="but-outline" onClick={this.start}>Inicia Ahora</button>
                         </div>                        
                     </div>
                     <div className="col-12 col-md-6 d-flex justify-content-center">
-                        <img className="banner" src="https://i.postimg.cc/wTRpFRBb/plant.png" alt="Imagen de una planta pequeña" />
+                        <img className="banner my-5 my-md-0" src="https://i.postimg.cc/85VcV8yM/plant.png" alt="Imagen de una planta pequeña" />
                     </div>
                 </div>
             </div>
--- imports/ui/Home/Navbar.js
+++ imports/ui/Home/Navbar.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
-    state = {  }
+    
     render() { 
         return ( 
             <nav className="fixed-top navbar navbar-expand-md bg-white shadow navbar-light">
                 <a className="navbar-brand d-flex align-items-center ml-3" href="/">
-                    <img src="img/leaf.svg" className="d-inline-block mr-2" width="40" height="40" />
+                    <img src="img/leaf.svg" className="d-inline-block mr-2" width="45" height="45" alt="LeafStyle logo" />
                     <span className="prefix">Leaf</span>Style
                 </a>
+                <button className="navbar-toggler mr-md-4" type="button" data-toggle="collapse" data-target="#thebar" aria-controls="thebar" aria-expanded="false" aria-label="Toggle Navigation Menu">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div id="thebar" className="collapse navbar-collapse justify-content-end mr-md-4">
+                    <ul className="navbar-nav ml-0 align-items-end">
+                        <li className="nav-item mx-md-2"><a href="/" className="nav-link">Inicio</a></li>
+                        <li className="nav-item dropdown ml-5">
+                            <button className="but-solid" id="drop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Entrar
+                            </button>
+                            <div className="dropdown-menu dropdown-menu-sm-right" aria-labelledby="drop">
+                                <a className="dropdown-item" href="/">Login</a>
+                                <a className="dropdown-item" href="/">Registro</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         );
     }
