65f4a8658e44e846f2fe2a26ad70b817042e675e
Andrés Losada <nedrocoli@gmail.com>
2019-10-14 14:51:13 -0500

    arreglado enviar la id al objeto

--- imports/ui/Evaluacion.js
+++ imports/ui/Evaluacion.js
@@ -3,6 +3,7 @@ import { withTracker } from "meteor/react-meteor-data";
 import Evaluaciones from "../api/evaluaciones";
 import Categoria from "../ui/Categoria";
 import Objeto from "../ui/Objeto";
+import objs from "../api/objetos";
 
 class Evaluacion extends Component {
   constructor(props) {
@@ -10,36 +11,36 @@ class Evaluacion extends Component {
   }
 
   state = {
-    objetos : this.props.objetos
+    objetos : this.props.objetos,
+    id: Evaluaciones.insert({fecha: new Date(), planetas: -1, _idUsuario: null, _idEvaluacionAnterior: null })
   }
 
   render() {
-    let id = Evaluaciones.insert({fecha: new Date(), planetas: -1, _idUsuario: null, _idEvaluacionAnterior: null })
-    return (
-      <div className="host">
+      return(<div className="host">
         <div className="container">
           <div className="row">
             <div className="col-6">
-              <Categoria id_Reporte= {id} />
+              <Categoria _idReporte= {this.state.id} />
             </div>
             <div className="col-6">
               <button type="button" onClick={() => {
-                this.calcPuntos(id)
+                console.log(this.state.id);
+                this.calcPuntos(this.state.id)
               }}>Calcular</button>
             </div>
           </div>
         </div>
       </div>
-    );
+      );
   }
 
-  calcPuntos() {
+  calcPuntos(id) {
     let sum = 0;
-    Evaluaciones.findOne(id);
+    let objetos = objs.find({_idReporte: id});
     objetos.map((obj, i) => {
       sum += obj.peso;
       console.log(sum);
-    })
+    });
   }
 
 }
