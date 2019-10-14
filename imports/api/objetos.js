4cb0c256e6feb82ef8f2914ea2c95bfcda9d44ad
vianaernesto <le.viana@uniandes.edu.co>
2019-10-14 12:37:01 -0500

    Objeto inserta, esperando a como funciona evaluación

--- imports/api/objetos.js
+++ imports/api/objetos.js
@@ -10,9 +10,9 @@ function insertObjeto(id,nombre, peso, descripcion, unidad, cantidad, _idCategor
 function insertAll(){
 
     if(Objetos.find().count() === 0){
-        insertObjeto("507f191e810c19729de860ea","Lavadora", 10, "Uso en horas de la lavadora", "Hora(s)", 5, "507f191e810c19729de860ea");
-        insertObjeto("507f191e810c19729de860eb","Agua", 20, "Uso en litros de agua", "Litros", 25, "507f191e810c19729de860ea");
-        insertObjeto("507f191e810c19729de860ec","Gas", 15.1, "Uso en libras de de gas", "Libras", 12, "507f191e810c19729de860ea");
+        insertObjeto("1","Lavadora", 10, "Uso en horas de la lavadora", "Hora(s)", 5, "1");
+        insertObjeto("2","Agua", 20, "Uso en litros de agua", "Litros", 25, "1");
+        insertObjeto("3","Gas", 15.1, "Uso en libras de de gas", "Libras", 12, "1");
     }
 }
 
--- imports/ui/Objeto.js
+++ imports/ui/Objeto.js
@@ -10,20 +10,20 @@ class Objeto extends Component{
     }
 
     handleNumber(x,i){
-        console.log(this.props.objetos);
-        console.log(this.state.objetos);
         var newObjeto = this.state.objetos[i];
         newObjeto.cantidad = x.target.value;
         this.setState(this.state.objetos[i] = newObjeto);
         
     }
     handleSubmit(objetoNuevo){
-        var id = objetoNuevo._id;
-        objetoNuevo._id = id;
-        objetos.update({_id : id},objetoNuevo);
+        var obj = objetoNuevo;
+        delete obj["_id"];
+        objetos.insert(obj)
         toast.success('Agregaste un uso de '+ objetoNuevo.cantidad + " " + objetoNuevo.unidad + " de " + objetoNuevo.nombre);
     }
     render(){
+
+
         return (
            <div className="row">
                {this.state.objetos.map((obj,i)=>{
@@ -32,9 +32,9 @@ class Objeto extends Component{
                         <div className="card-body">
                             <h5 className="card-title">{obj.nombre}</h5>
                             <h6 className="card-subtitle mb-2 text-muted">Peso: {obj.peso}</h6>
-                            <p className="card-text" style={{fontFamily: 'Exo 2'}} >{obj.descripcion}</p>
+                            <p className="card-text">{obj.descripcion}</p>
                             <input id="quantity" type="number" name="quantity" min="1" value={this.state.objetos[i].cantidad} onChange={(e) => {this.handleNumber(e,i)}}/>
-                            <label htmlFor="quantity" style={{fontfamily: 'Exo 2'}}>{obj.unidad}</label>
+                            <label htmlFor="quantity">{obj.unidad}</label>
                             <button className="but-solid" onClick={()=> {this.handleSubmit(this.state.objetos[i])}} style={{ marginTop: "3%"}}>Agregar</button>
                         </div>
                     </div>
