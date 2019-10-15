182335475f295afd3d91ba07b5e6117e8feafb2a
vianaernesto <le.viana@uniandes.edu.co>
2019-10-15 02:03:32 -0500

    Embellecer Evaluaciones

--- imports/ui/Usuario/ListarEvaluaciones.js
+++ imports/ui/Usuario/ListarEvaluaciones.js
@@ -16,18 +16,30 @@ class ListarEvaluaciones extends Component {
 
     render() {
         return (
-            <div className="container host">
+            <div className="container-fluid host">
+                <div className="row"><img
+                    src="https://www.pinclipart.com/picdir/big/112-1128532_eco-green-tree-png-clipart-transparent-png.png"
+                    alt="Logo para mostrar las evaluaciones realizadas."
+                    className="rounded-circle"
+                    height="80"
+                    width="80"
+                    style={{display:"inline-block", verticalAlign:"middle"}}
+                  />
+                <h2 class="font-weight-bold" style={{display:"inline-block", verticalAlign:"middle"}}>Tus Evaluaciones</h2></div>
+                
+                <br />
+                <br />
                 <ul className="list-group list-group-flush mb-5">
                     {this.state.evaluaciones.map((e, i) => {
                         if (this.props.idUsuario === e._idUsuario) {
                             return (
                                 <li key={i}
                                     className="list-group-item d-flex flex-md-row flex-column justify-content-between align-items-center">
-                                    <strong>Planetas: {e.planetas.toFixed(2)}</strong>
-                                    <strong>Fecha</strong>
-                                    <strong max-width="150px">{e.fecha.getFullYear()}/{e.fecha.getMonth()}/{e.fecha.getDate()}</strong>
-                                    <strong>Hora:</strong>
-                                    <strong max-width="150px">{e.fecha.getHours()}:{e.fecha.getMinutes()}</strong>
+                                    <strong><h5>Planetas: {e.planetas.toFixed(2)}  </h5></strong>
+                                    <strong><h5>Fecha:</h5></strong>
+                                    <strong max-width="150px"><h5>{e.fecha.getFullYear()}/{e.fecha.getMonth()}/{e.fecha.getDate()}</h5></strong>
+                                    <strong><h5>Hora:</h5></strong>
+                                    <strong max-width="150px"><h5>{e.fecha.getHours()}:{e.fecha.getMinutes()}</h5></strong>
                                 </li>
                             );
                         }
