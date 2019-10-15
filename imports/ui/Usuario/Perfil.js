6b195da1c11c823d4fab9926470ac9ac7ec717ae
Juan Camilo Pulido <jc.puido@uniandes.edu.co>
2019-10-15 00:46:19 -0500

    Avances en perfil

--- imports/ui/Usuario/Perfil.js
+++ imports/ui/Usuario/Perfil.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import Usuarios from '/imports/api/usuarios';
+import { toast } from 'react-toastify';
+
 
 import './Perfil.css';
 import ListarEvaluaciones from './ListarEvaluaciones';
@@ -9,47 +12,68 @@ class Perfil extends Component {
     constructor(props) {
         super(props);
 
+        this.handleCerrarSesion = this.handleCerrarSesion.bind(this)
+
+        this.state = {
+            redirect: false
+        }
+    }
+
+    renderRedirect() {
+        if (this.state.redirect) {
+            return <Redirect to="/" />
+        }
+    }
+
+    setRedirect() {
+        this.setState({
+            redirect: true
+        })
+    }
+
+    handleCerrarSesion() {
+        this.props.removeUsuario()
+        this.setRedirect()
     }
 
     render() {
         return (
-            <div className="container host">
-                <div className="row justify-content-center align-items-center">
-                    {/* <div className="col-4 col-md-5 mr-0 mr-md-3"> */}
-                    <div className="col-5">
-                        <div className="container host">
-                            <div className="row justify-content-center">
-                                <div className="col text-center">
-                                    <img className="rounded-circle" alt="100x100" src={this.props.getUsuario().imagen}
-                                        data-holder-rendered="true"></img>
-                                    <p className="puesto">
-                                        Puesto #1
-                                    </p>
-                                    <p className="nombre">
-                                        {this.props.getUsuario().nombre}
-                                    </p>
-                                    <p className="nombre-usuario">
-                                        @{this.props.getUsuario().nombreUsuario}
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="row justify-content-center">
-                                <div className="col">
-                                    <p className="profile-body">
-                                        Nombre de usuario: {this.props.getUsuario().nombreUsuario}
-                                    </p>
-                                    <p className="profile-body">
-                                        Correo: {this.props.getUsuario().correo}
-                                    </p>
+            <div>
+                {this.renderRedirect()}
+                <div className="container host">
+                    <div className="row justify-content-center align-items-center">
+                        {/* <div className="col-4 col-md-5 mr-0 mr-md-3"> */}
+                        <div className="col-5">
+                            <div className="container host">
+                                <div className="row justify-content-center">
+                                    <div className="col text-center">
+                                        <img className="rounded-circle" alt="100x100" src={this.props.getUsuario().imagen}
+                                            data-holder-rendered="true"></img>
+                                        <div className="puesto">
+                                            Puesto #1
+                                        </div>
+                                        <div className="nombre">
+                                            {this.props.getUsuario().nombre}
+                                        </div>
+                                        <div className="nombre-usuario">
+                                            @{this.props.getUsuario().nombreUsuario}
+                                        </div>
+                                        <div className="correo">
+                                            {this.props.getUsuario().correo}
+                                        </div>
+                                        <div className="pt-2">
+                                            <button onClick={this.handleCerrarSesion}><link to="/"></link>Cerrar sesión</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    {/* <div className="col-8 col-md-7 ml-0 ml-md-3"> */}
-                    <div className="col-7">
-                        {/* Aquí van sus amigos listados */}
-                        <div className="row">
-                            <ListarEvaluaciones {... this.props.getUsuario()._id}/>
+                        {/* <div className="col-8 col-md-7 ml-0 ml-md-3"> */}
+                        <div className="col-7">
+                            {/* Aquí van sus amigos listados */}
+                            <div className="row">
+                                <ListarEvaluaciones {... this.props.getUsuario()._id} />
+                            </div>
                         </div>
                     </div>
                 </div>
