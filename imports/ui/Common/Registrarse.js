fe8249499edcb1c141d73a710bddd6d8c5dec60a
Santiago Salazar <s.salazara@uniandes.edu.co>
2019-10-14 22:54:37 -0500

    Cambios locales para el ListarAmigos.js. Aún trabajando.

--- imports/ui/Common/Registrarse.js
+++ imports/ui/Common/Registrarse.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { toast } from 'react-toastify';
 
 import './Auth.css';
--- imports/ui/Usuario/ListarAmigos.js
+++ imports/ui/Usuario/ListarAmigos.js
@@ -1,36 +1,59 @@
 import React, { Component } from 'react';
 
+import './Usuario.css';
+import Usuarios from '/imports/api/usuarios'
+
 class ListarAmigos extends Component {
     
     constructor(props) {
         super(props);
+
+        let f = this.props.amigos.filter(x => this.props.getUsuario().amigos.includes(x._id) || this.props.getUsuario()._id === x._id);
         this.state = {
-            busqueda: ""
+            busqueda: "",
+            lista: f,
+            original: f
         }
 
         this.filtrar = this.filtrar.bind(this);
     }
-
+    
     filtrar(event) {
-
+        const b = event.target.value;
+        let filtrado = this.state.original.filter(x => x._id.includes(b));
+        this.setState({ 
+            busqueda: b,
+            lista: filtrado
+         });
     }
 
-    validate() {
-        return this.state.busqueda.length > 0;
+    listarAmigos() {
+        let ordenada = this.state.lista.sort( (a1, a2) => a2.ahorroActual - a1.ahorroActual);
+        return ordenada.map( (e,i) => {
+            return (
+                <li key={i}>
+                    Content!
+                </li>
+            );
+        });
     }
 
     render() { 
         return ( 
             <div className="host">
-                <div className=""></div>
-                <input id="buscarAmigos" autoFocus type="text" value={this.busqueda} onChange={this.filtrar} />
-                <button className="but-solid" disabled={!this.validate()}>Buscar</button>
-                <div>
-
+                <div className="my-4 buscador">
+                    <input className="form-control" id="buscarAmigos" autoFocus type="text" value={this.state.busqueda} onChange={this.filtrar} placeholder="Busca por nombre de usuario" />
                 </div>
+                <ul className="list-group list-group-flush">
+                    {this.listarAmigos()}
+                </ul>
             </div>
         );
     }
 }
  
-export default ListarAmigos;
+export default withTracker(() => {
+    return {
+      amigos: Usuarios.find().fetch(),
+    };
+  })(ListarAmigos);;
