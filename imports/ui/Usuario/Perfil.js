a9a8056387131184eb6491eaca3411403052286d
Juan Camilo Pulido <jc.puido@uniandes.edu.co>
2019-10-14 22:48:03 -0500

    vista perfil medio implementada

--- imports/ui/Usuario/Perfil.js
+++ imports/ui/Usuario/Perfil.js
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
 import Usuarios from '/imports/api/usuarios';
 
+import './Perfil.css';
+
 class Perfil extends Component {
-    
+
     constructor(props) {
         super(props);
 
     }
-    
-    render() { 
-        return ( 
+
+    render() {
+        return (
             <div className="container host">
                 <div className="row justify-content-center align-items-center">
-                    <div className="col-12 col-md-5 mr-0 mr-md-3">
-                        {/* Aquí va el perfil (datos) del usuario */}
-                        
-                    </div>
-                    <div className="col-12 col-md-7 ml-0 ml-md-3">
-                        {/* Aquí van sus amigos listados */}
+                    {/* <div className="col-4 col-md-5 mr-0 mr-md-3"> */}
+                    <div className="col-5">
+                        <div className="container host">
+                            <div className="row justify-content-center">
+                                <div className="col center-block">
+                                    <img className="rounded-circle" alt="100x100" src="https://randomuser.me/api/portraits/men/95.jpg"
+                                        data-holder-rendered="true"></img>
 
+                                    <p className="nombre">
+                                        {this.props.getUsuario().nombre}
+                                    </p>
+                                    <p className="puesto">
+                                        Puesto #1
+                                </p>
+                                </div>
+                            </div>
+                            <div className="row justify-content-center">
+                                <div className="col">
+                                    <p className="profile-body">
+                                        Nombre de usuario: {this.props.getUsuario().nombreUsuario}
+                                    </p>
+                                    <p className="profile-body">
+                                        Correo: {this.props.getUsuario().correo}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div className="col-8 col-md-7 ml-0 ml-md-3"> */}
+                    <div className="col-7">
+                        {/* Aquí van sus amigos listados */}
                     </div>
                 </div>
             </div>
         );
     }
 }
- 
+
 export default Perfil;
